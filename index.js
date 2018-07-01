d49dc8b239b6ba42eb2ee827c190e850e82d4a30
Mystogan98 <shangheilis@hotmail.fr>
2018-07-01 16:34:11 +0200

    Arghblabla

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const bot 		= new Discord.Client();
 //----------↑↑ Constantes ↑↑----------
 
 var prefix 		= settings.prefix;
-var message;
 
 //----------↑↑ Variables ↑↑----------
 //----------↓↓    Code   ↓↓----------
@@ -22,7 +21,6 @@ bot.on('ready', function() {
 
 // S'execute lorsque le bot recoit un message (un message sur le channel)
 bot.on('message', message => {
-	message = message;
 	// Ignore les autres bots (pas de botception !)
 	if(message.author.bot) return;
 	// Prefixes obligatoire pour les commandes du bot
@@ -38,52 +36,54 @@ bot.on('message', message => {
 	{
 		// "help" -> Affiche la liste des commandes
 		case 'help' :
-			help();
+			help(message);
 		break;
 		// "salut" -> Affiche "Bien le bonjour ! :)"
 		case 'salut' :
-			salut();
+			salut(message);
         break;
         // "goat" -> Affiche un gif d'une chevre
         case "goat" :
-        	goat();
+        	goat(message);
         break;
         // "licorne" -> Affiche l'image d'une licorne
         case "licorne" :
-        	licorne();
+        	licorne(message);
 		break;
 		// "prefixe" -> change le préfixe
 		case "prefixe" :
-			prefixe(parametre.split(" ")[0]);
+			prefixe(message,parametre.split(" ")[0]);
 		break;
 	}
 });
 
-function help() {
-	message.channel.sendMessage("Liste des commandes:\n -\\"+prefix+"help\t Affiche la liste des commandes\n-"+prefix+"salut\t Vous souhaite la bienvenue\n-"+prefix+"goat\t Affiche le gif d'une super chevre\n-\\"+prefix+"licorne\t Affiche la meilleure licorne du monde\n\nPour plus d'aide, taper "+prefix+"help [nom de la commande]");
+function help(message) {
+	message.channel.send("Liste des commandes:\n -\\"+prefix+"help\t Affiche la liste des commandes\n-"+prefix+"salut\t Vous souhaite la bienvenue\n-"+prefix+"goat\t Affiche le gif d'une super chevre\n-\\"+prefix+"licorne\t Affiche la meilleure licorne du monde\n\nPour plus d'aide, taper "+prefix+"help [nom de la commande]");
 	console.log("Commande help affichée");
 }
 
-function salut() {
+function salut(message) {
 	message.reply("Bien le bonjour ! :)");
 	console.log("Commande Salut effectuée.");
 }
 
-function goat() {
+function goat(message) {
 	message.reply("https://media.giphy.com/media/MYEgNY8dIxPpe/giphy.gif");
 	console.log("Summoned a goat !");
 }
 
-function licorne() {
+function licorne(message) {
 	message.channel.send("https://cdn.discordapp.com/attachments/282472433313644544/462945595955478528/licorne.jpg");
 	console.log("La meilleure licorne du monde à été affichée.");
 }
 
-function prefixe(newPrefixe) {
+function prefixe(message,newPrefixe) {
 	// On change le prefixe dans notre variable
 	prefix = newPrefixe;
 	// Puis dans le fichier
 	config.prefix = newPrefixe;
 	// Et enfin on sauvegarde le fichier
 	fs.writeFile("./settings.json",JSON.stringify(config), err => console.error);
+	// On l'indique dans le channel
+	message.channel.send("Nouveau préfixe : " + newPrefixe);
 }
