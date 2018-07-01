aa991cec2d6a092270aab5383c6e74ee219a5dc5
Mystogan98 <shangheilis@hotmail.fr>
2018-07-01 16:27:35 +0200

    Ajout de la commande prefixe

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const Discord 	= require('discord.js');
-const settings 	= require("settings.json");
+const settings 	= require('./settings.json');
 const fs 		= require("fs");
 const bot 		= new Discord.Client();
 
 //----------↑↑ Constante ↑↑----------
 
-var prefix 		= settings.prefix
+var prefix 		= settings.prefix;
 
 //----------↑↑ Variable ↑↑----------
 //----------↓↓   Code   ↓↓----------
@@ -15,7 +15,7 @@ bot.login(settings.login);
 // S'execute lors de la mise en route du bot
 bot.on('ready', function() {
 	// Modifie le jeu du bot
-    bot.user.setGame("Comand: *help");
+    bot.user.setActivity("Command: *help");
     console.log("Connected");
 });
 
@@ -29,34 +29,59 @@ bot.on('message', message => {
 	message.content = message.content.substring(1);
 
 	// Et enfin on decoupe notre message
-	var commande = message.content.split(" ")[0];					// On decoupe selon les espaces puis on récupère le premier mot (la commande donc)
-	var parametre = message.content.split(" ").slice(1).tostring(); // On decoupe selon les espaces puis on recupère tous les autres mots sous forme de chaine de caractères
+	var commande = message.content.split(" ")[0];						// On decoupe selon les espaces puis on récupère le premier mot (la commande donc)
+	var parametre = message.content.split(" ").slice(1).toString(); 	// On decoupe selon les espaces puis on recupère tous les autres mots sous forme de chaine de caractères
 
 	switch(commande) 
 	{
 		// "help" -> Affiche la liste des commandes
 		case 'help' :
-			message.channel.sendMessage("Liste des commandes:\n -\\*help\t Affiche la liste des commandes\n-salut\t Vous souhaite la bienvenue\n-goat\t Affiche le gif d'une super chevre\n-\\*licorne\t Affiche la meilleure licorne du monde");
+			help();
 		break;
 		// "salut" -> Affiche "Bien le bonjour ! :)"
 		case 'salut' :
-			message.reply("Bien le bonjour ! :)");
-        	console.log("Commande Salut effectuée.");
+			salut();
         break;
         // "goat" -> Affiche un gif d'une chevre
         case "goat" :
-        	message.reply("https://media.giphy.com/media/MYEgNY8dIxPpe/giphy.gif");
-        	console.log("Summoned a goat !");
+        	goat();
         break;
         // "licorne" -> Affiche l'image d'une licorne
         case "licorne" :
-        	message.channel.send("https://cdn.discordapp.com/attachments/282472433313644544/462945595955478528/licorne.jpg");
-    		console.log("La meilleure licorne du monde à été affichée.");
-    	break;
+        	licorne();
+		break;
+		// "prefixe" -> change le préfixe
+		case "prefixe" :
+			prefixe(parametre.split(" ")[0]);
+		break;
 	}
 });
 
-//function help() {
-	//message.channel.sendMessage("Liste des commandes:\n");
-//}
+function help() {
+	message.channel.sendMessage("Liste des commandes:\n -\\"+prefix+"help\t Affiche la liste des commandes\n-"+prefix+"salut\t Vous souhaite la bienvenue\n-"+prefix+"goat\t Affiche le gif d'une super chevre\n-\\"+prefix+"licorne\t Affiche la meilleure licorne du monde\n\nPour plus d'aide, taper "+prefix+"help [nom de la commande]");
+	console.log("Commande help affichée");
+}
 
+function salut() {
+	message.reply("Bien le bonjour ! :)");
+	console.log("Commande Salut effectuée.");
+}
+
+function goat() {
+	message.reply("https://media.giphy.com/media/MYEgNY8dIxPpe/giphy.gif");
+	console.log("Summoned a goat !");
+}
+
+function licorne() {
+	message.channel.send("https://cdn.discordapp.com/attachments/282472433313644544/462945595955478528/licorne.jpg");
+	console.log("La meilleure licorne du monde à été affichée.");
+}
+
+function prefixe(newPrefixe) {
+	// On change le prefixe dans notre variable
+	prefix = newPrefixe;
+	// Puis dans le fichier
+	config.prefix = newPrefixe;
+	// Et enfin on sauvegarde le fichier
+	fs.writeFile("./settings.json",JSON.stringify(config), err => console.error);
+}
